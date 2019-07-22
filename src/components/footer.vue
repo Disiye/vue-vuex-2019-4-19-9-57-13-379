5924d7ce0f0c605959720b4836abbd05edea7944
WENGEM <emilio.weng@oocl.com>
2019-07-22 20:08:29 +0800

    comlete footer and header

---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+      <span class="todo-count"><strong>{{unComplete}}</strong> item left</span>
+      <ul class="filters">
+        <li>
+          <a class="selected">All</a>
+        </li>
+        <li>
+          <a>Active</a>
+        </li>
+        <li>
+          <a>Completed</a>
+        </li>
+      </ul>
+      <button class="clear-completed" @click="clearCompletedAsync">Clear completed</button>
+    </footer>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: mapGetters(['unComplete']),
+  methods: mapActions(['clearCompletedAsync'])
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input class="new-todo" placeholder="What needs to be done?" autofocus v-model="value" @keyup.enter="addList">
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value:'',
+    }
+  },
+  methods: {
+    addList () {
+      this.$store.commit('addList',{text:this.value})
+      this.value = ""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
